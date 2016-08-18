52c87026fddfec0bdc2c329e917204af19f7de45
Matthew Barnett <fishsaucen@gmail.com>
2016-08-18 11:42:01 -0500

    included functions to process a date entered by the user

--- main.py
+++ main.py
@@ -15,26 +15,92 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
 
 form="""
-     <form method="post" action="/testform">
-        <input name="q">
-        <input type="submit">
-     </form>
-     """
+<form method="post">
+    What is your birthday?
+    <br/>
+    <label> Month
+        <input type="text" name="month" value="%(month)s">
+    </label>
+
+    <label> Day
+        <input type="text" name="day" value="%(day)s">
+    </label>
+
+    <label> Year
+        <input type="text" name="year" value="%(year)s">
+    </label>
+    <div style="color: red">%(error)s</div>
+    <br>
+    <br>
+<input type="submit">
+</form>
+"""
+# crazy cool string substitution
+string = "I can substitute %s all day"
+# returns "I can substitute strings all day"
+# return string % strings
+# named substitution
+# "text %(NAME)s text" % {"NAME": value}
+months = ["January", "February", "March", "April",
+          "May", "June", "July", "August", "September",
+          "October", "November", "December"]
+
+def escape_html(s):
+    # alternatively, use cgi.escape(s, quote=True)
+    for (i, o) in (("&", "&amp;"),
+                   (">", "&gt;"),
+                   ("<", "&lt;"),
+                   ('"', "&quot;")):
+        s = s.replace(i, o)
+    return s
+
+def valid_month(month):
+    if month.capitalize() in months:
+        return month.capitalize()
+
+def valid_day(day):
+    if day.isdigit():
+        day = int(day)
+        if day > 0 and day < 32:
+            return day
+
+def valid_year(year):
+    if year.isdigit():
+        year = int(year)
+        if year >= 1900 and year <= 2020:
+            return year
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write(form)
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error, "month": escape_html(month),
+                                        "day": escape_html(day), "year": escape_html(year)})
 
-class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        self.write_form()
+
     def post(self):
-        #q = self.request.get("q")
-        #self.response.write(q)
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(self.request)
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
+
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+
+        if not (month and day and year):
+            self.write_form("Invalid input, try again.", user_month, user_day, user_year) 
+        else:
+            self.redirect("/thanks") 
+
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Thanks! that's a valid day!")
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/testform', TestHandler)
+    ('/thanks', ThanksHandler),
 ], debug=True)
